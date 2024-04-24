e92a5db6779581076f8597250baa2dbfde9e9903
rm <jossuerm55@gmail.com>
2024-04-23 22:01:35 -0600

    Cargar productos

---
+++ cargar_productos/cargar_productos.js
@@ -0,0 +1,28 @@
+
+function Cargar_productos(products_list){
+    let div_product_list = document.querySelector("#div_product_list");
+
+    products_list.forEach(product => {
+        
+        let div = document.createElement("div");
+        div.classList = "item_product";
+
+        let short_text = product.title.split(' ').slice(0, 5).join(' ');
+        console.log(short_text);
+
+        div.innerHTML = `
+            <div class="box_img">
+                <img src="${product.image}" alt="">
+            </div>
+            <div class="box_description">
+                <span class="name">${short_text}</span>
+                <span class="price">Q ${product.price}</span>
+                <div class="buy_btn" id="${product.id}">Comprar</div>
+            </div>
+        `;
+        div_product_list.appendChild(div);
+    });
+
+}
+
+export { Cargar_productos }
---
+++ main.js
@@ -0,0 +1,18 @@
+import { Cargar_productos } from "/cargar_productos/cargar_productos.js";
+
+let DOM = document.querySelector("#root");
+DOM.innerHTML = `
+    <header></header>
+    <main class="body">
+        <section id="div_product_list"></section>
+        <section id="div_summery_description"></section>
+    </main>
+    <footer></footer>
+`;
+
+fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(data => {
+        let productos_api = data;
+        Cargar_productos(productos_api);
+    })
